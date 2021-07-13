9aaa577e198f35b473f1a614e24b7c4581f2d576
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2021-07-13 15:58:14 +0530

    there we go

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "this =00=script is from github"
+echo "this final=00=script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "===This is new one i am here"
